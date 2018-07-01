42c37650721201b122a1ec5e74c54107df29fee3
Zistack <Zistack@null.net>
2018-06-30 19:28:31 -0700

    Introduced mechanism to specify inter-module dependencies

--- src/libs/namespace.sh
+++ src/libs/namespace.sh
@@ -1,9 +1,45 @@
 #INCLUDE (libs/list.sh)
 
+function process-module
+{
+	local module="${1}"
+
+	for dependency in $(cat "${module}"/dependencies 2>/dev/null)
+	do
+		conditionally-process-module "${dependency}"
+	done
+
+	local new_path="${path}"'/'"${module}"
+
+	namespace-do "${module}" "${new_path}" "${command}"
+}
+
+function conditionally-process-module
+{
+	local module="${1}"
+
+	if ! grep -q "${module}" <<< "${processed_modules}"
+	then
+		processed_modules="${processed_modules}${module}\n"
+
+		process-module "${module}"
+	fi
+}
+
+function process-modules
+{
+	local processed_modules=""
+
+	for new_module in $(list-modules)
+	do
+		conditionally-process-module "${new_module}"
+	done
+}
+
 function namespace-do
 {
-	local path="${1}"
-	local module="${2}"
+	local module="${1}"
+	local path="${2}"
 	local command="${3}"
 
 	echo 'namespace '"${module}"
@@ -11,12 +47,7 @@ function namespace-do
 
 	cd "${module}"
 
-	for new_module in $(list-modules)
-	do
-		local new_path="${path}"'/'"${new_module}"
-
-		namespace-do "${new_path}" "${new_module}" "${command}" | sed -e 's~^~	~'
-	done
+	process-modules | sed -e 's~^~	~'
 
 	"${command}" "${path}"
 
